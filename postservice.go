c3b4da320d64133aa4aaa578e3a2e1b07078fed1
UdayPratap Singh <udaypratap.singh@eagleview.com>
2020-11-22 13:02:10 +0530

    Initial commit

---
+++ postservice.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"bytes"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "54321"
+	dbname   = "postgres"
+)
+
+//DB global DB variable .
+var DB *sql.DB
+
+//User reference type for UserService
+type User struct {
+	UserID       int
+	IncPostCount bool
+}
+
+//PostModel to update postTable
+type PostModel struct {
+	ID          int    `json:"id"`
+	UserID      int    `json:"UserId"`
+	Title       string `json:"Title"`
+	Description string `json:"Description"`
+}
+
+func main() {
+	e := echo.New()
+	e.DELETE("/deletePost", PostDelete)
+	e.POST("/createPost", Create)
+	initDB()
+	e.Logger.Fatal(e.Start(":8002"))
+	defer DB.Close()
+}
+func initDB() {
+	var err error
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	DB, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatalf("Error in connecting Db %v", err)
+		panic(err)
+	}
+}
+
+//Create to handle post creation
+func Create(c echo.Context) error {
+	post := PostModel{}
+	defer c.Request().Body.Close()
+	err := json.NewDecoder(c.Request().Body).Decode(&post)
+	if err != nil {
+		log.Fatalf("Failed reading request body %s", err)
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error)
+	}
+	log.Printf("Post Creation Object %#v", post)
+	sqlStatement := `INSERT INTO public.post(id, "userId", title, description, created) sVALUES ($1, $2, $3, $4,$5)`
+	_, err = DB.Exec(sqlStatement, post.ID, post.UserID, post.Title, post.Description, time.Now())
+	if err != nil {
+		panic(err)
+	}
+
+	user := User{
+		UserID:       post.UserID,
+		IncPostCount: true,
+	}
+	// Initialize http client
+	client := &http.Client{}
+	// marshal User to json
+	json, err := json.Marshal(user)
+	if err != nil {
+		panic(err)
+	}
+	//set HTTP method, url, and request body
+	req, err := http.NewRequest(http.MethodPut, "http://localhost:8000/updatepost", bytes.NewBuffer(json))
+	if err != nil {
+		panic(err)
+	}
+	// set request header Content-Type for json
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	_, err = client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	return c.JSON(http.StatusOK, "Post Created ...")
+}
+
+//PostDelete to handle post deletion
+func PostDelete(c echo.Context) error {
+	post := PostModel{}
+	defer c.Request().Body.Close()
+	err := json.NewDecoder(c.Request().Body).Decode(&post)
+	if err != nil {
+		log.Fatalf("Failed reading the request body %s", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error)
+	}
+	log.Printf("Post Deletion Object %#v", post)
+	sqlStatement := `DELETE FROM post WHERE id = $1;`
+	_, err = DB.Exec(sqlStatement, post.ID)
+	if err != nil {
+		panic(err)
+	}
+
+	user := User{
+		UserID:       post.UserID,
+		IncPostCount: false,
+	}
+
+	// initialize http client
+	client := &http.Client{}
+
+	// marshal User to json
+	json, err := json.Marshal(user)
+	if err != nil {
+		panic(err)
+	}
+
+	// set HTTP method, url, and request body
+	req, err := http.NewRequest(http.MethodPut, "http://localhost:8000/updatepost", bytes.NewBuffer(json))
+	if err != nil {
+		panic(err)
+	}
+
+	// set request header Content-Type for json
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	_, err = client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	return c.JSON(http.StatusOK, "Post Deleted ...")
+}
---
+++ userservice.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo"
+	_ "github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
+)
+
+//DB global DB variable .
+var DB *sql.DB
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "54321"
+	dbname   = "postgres"
+)
+
+func main() {
+	e := echo.New()
+	e.POST("/login", LoginUser)
+	e.POST("/signup", SignUpUser)
+	e.PUT("/updatepost", UpdatePostCount)
+	initDB()
+	e.Logger.Fatal(e.Start(":8000"))
+	defer db.Close()
+}
+func initDB() {
+	var err error
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	DB, err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal("Error in connecting db")
+		panic(err)
+	}
+}
+
+//UserCredential for user login
+type UserCredential struct {
+	Email    string `jsonn:"email", db:"email"`
+	Username string `json:"username", db:"username"`
+	Password string `json:"password", db:"password"`
+}
+
+//LoginUser to allow login
+func LoginUser(c echo.Context) error {
+	creds := &UserCredential{}
+	err := json.NewDecoder(c.Request().Body).Decode(creds)
+	defer c.Request().Body.Close()
+	if err != nil {
+		log.Fatalf("Failed reading request body %s", err)
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error)
+	}
+	result := db.QueryRow("select password from user where name=$1 or email = $2", creds.Username, creds.Email)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error)
+	}
+	storedCreds := &UserCredential{}
+	err = result.Scan(&storedCreds.Password)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return echo.NewHTTPError(http.StatusUnauthorized, err.Error)
+		}
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error)
+	}
+	if err = bcrypt.CompareHashAndPassword([]byte(storedCreds.Password), []byte(creds.Password)); err != nil {
+
+		return echo.NewHTTPError(http.StatusUnauthorized, err.Error)
+	}
+	return c.JSON(http.StatusOK, "Successfully Logged In ...")
+}
+
+//SignUpUser to allow signing in
+func SignUpUser(c echo.Context) error {
+	creds := &UserCredential{}
+	err := json.NewDecoder(c.Request().Body).Decode(creds)
+	defer c.Request().Body.Close()
+	if err != nil {
+		log.Fatalf("Failed reading request body %s", err)
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error)
+	}
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(creds.Password), 8)
+	db.Exec("insert into user (name,email,password) values ($1, $2, $3)", creds.Username, creds.Email, string(hashedPassword))
+	return c.JSON(http.StatusOK, "Successfully Signed In ...")
+}
+
+//UpdatePostCount to update user posts
+func UpdatePostCount(c echo.Context) error {
+	incrementflag, _ := strconv.ParseBool(c.FormValue("postcountincr"))
+	userID := c.FormValue("userID")
+	result := db.QueryRow("select post_count from user where id=$1", userID)
+	var postcount int
+	err := result.Scan(&postcount)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return echo.NewHTTPError(http.StatusUnauthorized, err.Error)
+		}
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error)
+	}
+	if incrementflag {
+		postcount++
+	} else {
+		postcount--
+	}
+	db.Exec("update user set post_count = $1 where id=$2", postcount, userID)
+	return c.JSON(http.StatusOK, "PostCount Updated Successfully ...")
+}
